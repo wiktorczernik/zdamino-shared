e550ca692bb320891e3489868d2cd0a4f8b44629
Wiktor Czernik <git@czernik.dev>
2025-12-21 01:28:11 +0100

    Renamed Exam to ExamInfo, Task to TaksInfo, created RawExamInfo that represents examInfo.json file

---
+++ src/types/rawExamInfo.js
@@ -0,0 +1,13 @@
+import RawTaskInfo from "./rawTaskInfo.js"
+
+export default class RawExamInfo {
+    /** 
+     * @typedef {Object} RawExamParams
+     * @property {Array<RawTaskInfo>} tasks
+     */
+    /** @param {RawExamParams} params */
+    constructor(params) {
+        /** @type {Array<RawTaskInfo>} */
+        this.tasks = params.tasks
+    }
+}
---
+++ src/types/rawTaskInfo.js
@@ -0,0 +1,16 @@
+import { TaskType } from "../enums/taskType.js"
+
+export default class RawTaskInfo {
+    /** 
+     * @typedef {Object} RawTaskParams
+     * @property {string} title
+     * @property {TaskType} type
+     */
+    /** @param {RawTaskParams} params */
+    constructor(params) {
+        /** @type {string} */
+        this.title = params.title;
+        /** @type {TaskType} */
+        this.type = params.type;
+    }
+}
